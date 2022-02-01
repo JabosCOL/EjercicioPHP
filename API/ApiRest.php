f6e3b14c7c48ad2a49e3af56e5f7a2d664a2e578
Cesar David Bolivar Cuchigay <c.bolivarcuchigay@uniandes.edu.co>
2022-02-01 11:59:59 -0500

    Petición POST

--- API/ApiRest.php
+++ API/ApiRest.php
@@ -14,4 +14,34 @@
 
 // curl_close($ch);
 
+$ch = curl_init();
+
+$array = [
+    'first_name' => 'David',
+    'last_name' => 'Bolivar',
+    'job' => 'Junior Programmer'
+    
+];
+
+$data = http_build_query($array);
+
+curl_setopt($ch, CURLOPT_URL, 'https://reqres.in/api/users');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch))
+echo curl_error($ch);
+
+else $decoded = json_decode($response, true);
+
+foreach ($decoded as $index => $value)
+{
+    echo "$index: $value <br>";
+}
+
+curl_close($ch);
+
 ?>
